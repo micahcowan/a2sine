e69f6cebe5cb8bffde0695aefe5fb6fa0b1f4a1e
Micah Cowan <micah@addictivecode.org>
2023-01-07 15:53:02 -0800

    SaveAndInstallZp

--- sinecalc.s
+++ sinecalc.s
@@ -27,42 +27,46 @@ XXXPreloadZp:
         sta XXXPreloadZp
 	rts
 
+;; NOT RE-ENTRANT
 SineCalcRunReal:
-	pha
-        txa
-        pha
-        jsr XXXPreloadZp
-	jsr SaveZp
-        pla
-        tax
-        pla
-        ; First, copy A, Y addr into ZP
-        sta $0
-        sty $1
-        ; Push X back onto stack
-        txa
-        pha
-            ; Copy timers addr into ZP
-            ldy #2
-            lda ($0),y
-            sta $2
-            iny
-            lda ($0),y
-            sta $3
-            ; Now copy commands addr, overwriting $0
-            ldy #0
-            lda ($0),y
-            tax
-            iny
-            lda ($0),y
-            stx $0
-            sta $1
+	sta SavedA
+        stx SavedX
+        sty SavedY
+	jsr XXXPreloadZp
+        jsr SaveAndInstallZp
+        nop
         jsr RestoreZp
-        pla
-        tax
 	rts
+SavedA:
+	.byte 0
+SavedX:
+	.byte 0
+SavedY:
+	.byte 0
         
 SaveAndInstallZp:
+	jsr SaveZp
+        ; First, copy A, Y addr into ZP
+        lda SavedA
+        sta $0
+        ldy SavedY
+        sty $1
+        ; Copy timers addr into ZP
+        ldy #2
+        lda ($0),y
+        sta $2
+        iny
+        lda ($0),y
+        sta $3
+        ; Now copy commands addr, overwriting $0
+        ldy #0
+        lda ($0),y
+        tax
+        iny
+        lda ($0),y
+        stx $0
+        sta $1
+	rts
 
 SaveZp:
 	ldx #0
